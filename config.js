5ed5cec3728b91f4de1d1ced10e0f882d4ff9aa1
Rustam <rustam905@gmail.com>
2020-12-09 18:28:12 -0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-	'secretKey': "12345-67890-09876-54321",
-	'mongoUrl': "mongodb://localhost:27017/nucampsite",
+	secretKey: "12345-67890-09876-54321",
+	mongoUrl: "mongodb://localhost:27017/nucampsite",
+	facebook: {
+		clientId: "2734632390199168",
+		clientSecret: "8246af5a75b17820e024805b44ab152c",
+	},
 };
